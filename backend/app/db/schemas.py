62016c52db5adf6ef7e2aa798923fad33eccb213
zkdltid-chan <zkdltid.chan@gmail.com>
2023-05-25 01:40:54 +0900

    init

---
+++ backend/app/db/schemas.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel
+from pydantic.generics import GenericModel
+import typing as t
+
+T = t.TypeVar("T")
+
+class ResponseDataList(GenericModel, t.Generic[T]):
+    data_list: t.List[T]
+
+class UserBase(BaseModel):
+    email: str
+    is_active: bool = True
+    is_superuser: bool = False
+    first_name: str = None
+    last_name: str = None
+
+
+class UserOut(UserBase):
+    pass
+
+
+class UserCreate(UserBase):
+    password: str
+
+    class Config:
+        orm_mode = True
+
+
+class UserEdit(UserBase):
+    password: t.Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: str = None
+    permissions: str = "user"
